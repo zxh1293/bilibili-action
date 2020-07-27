667651a082147880f39f3b550a3c655d3b8056f4
kkkkkkkkssssss <68812417+kkkkkkkkssssss@users.noreply.github.com>
2020-07-27 14:55:01 +0800

    upload

---
+++ README.md
@@ -0,0 +1,47 @@
+# 哔哩哔哩直播自动签到
+> 哔哩哔哩直播自动签到
+
+## Github Actions 部署指南
+
+### 一、Fork 此仓库
+![image-20200727142541791](https://i.loli.net/2020/07/27/jK5H8FLvt7aBeYX.png)
+
+
+
+### 二、设置账号COOKIE
+先登录B站，然后访问https://api.live.bilibili.com/sign/GetSignInfo，按***F12***，在***Console***中输入***document.cookie***，复制结果（不含引号）。
+
+![image-20200727143957069](C:\Users\Administrator\AppData\Roaming\Typora\typora-user-images\image-20200727143957069.png)
+
+在***GitHub***中添加名为 **COOKIE**的变量，值为复制的内容。
+
+> Settings-->Secrets-->New secret
+
+支持多账号，账号**COOKIE**之间用 ***#*** 分隔
+
+示例：**COOKIE:sid=97d0....#sid=97d0....**
+![image-20200727142753175](https://i.loli.net/2020/07/27/xjri3p4qdchaf2G.png)
+
+### 三、启用 Action
+1. 点击 ***Actions***，再点击 **I understand my workflows, go ahead and enable them**
+
+   ![](http://tu.yaohuo.me/imgs/2020/06/34ca160c972b9927.png)
+
+2. 点击左侧的 ***Star***
+
+   ![image-20200727142617807](https://i.loli.net/2020/07/27/3cXnHYIbOxfQDZh.png)
+
+### 四、查看运行结果
+> Actions --> 签到 --> build
+>
+> 能看到如下图所示，表示成功
+
+![](https://i.loli.net/2020/07/27/YLkpXSbRj73nwov.png)
+
+## 注意事项
+
+1. 每天运行两次，在上午 6 点和晚上 22 点。
+
+2. 可以通过 ***Star*** 手动启动一次。
+
+   ![image-20200727142617807](https://i.loli.net/2020/07/27/87oQeLJOlZvU3Ep.png)
---
+++ checkin.py
@@ -0,0 +1,49 @@
+import requests, json,base64
+
+s = requests.Session ()
+
+cookie = ""
+if (cookie == ""):
+    cookie=input ().strip ()
+
+def main ():
+    url1 = f'https://api.live.bilibili.com/sign/GetSignInfo'
+    url2 = f'https://api.live.bilibili.com/sign/doSign'
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+        "Referer" : "https://live.bilibili.com/491741",
+        "Accept-Encoding" : "gzip, deflate",
+        "Cookie":cookie,
+    }
+    s.get (url1,headers=headers)
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+        "Referer" : "https://live.bilibili.com/491741",
+        "cookie": cookie,
+        "Accept-Encoding" : "gzip, deflate",
+    }
+    s.get (url2,headers=headers)
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+        "Referer" : "https://live.bilibili.com/491741",
+        "Accept-Encoding" : "gzip, deflate",
+        "Cookie":cookie,
+    }
+    
+    response=s.get (url1,headers=headers)
+    dec=json.loads (response.text)
+    if dec ['data']['status']==1:
+        print ("已签到！\n 签到奖励："+dec ['data']['text']+"\n"+dec ['data']['specialText']+"\n")
+    else:
+        if dec ['data']['status']==-101:
+            print ("失败！请检查 Cookie 是否过期！\n")
+            exit (100)
+        else:
+            print ("网络故障，请重试！\n")
+            exit (100)
+
+
+
+if __name__ == "__main__":
+    main ()
+
